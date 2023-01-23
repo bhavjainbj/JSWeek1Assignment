59e347fd45ad31b73080beb3dc780021be155be2
Bhavya Jain <63778065+bhavjainbj@users.noreply.github.com>
2023-01-23 23:01:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function checkForBlank(){
+    
+
+    if(document.getElementById('desc').value == ""){
+        alert("Enter the Description!!");
+        document.getElementById('desc').style.borderColor = 'red';
+        return false;
+    }
+    else if(document.getElementById('ass').value == ""){
+        alert("Enter the Assigned To!!");
+        document.getElementById('ass').style.borderColor = 'red';
+        return false;
+    }
+    else{
+        var x = document.getElementById("desc").value;
+        document.getElementById("descOp").innerHTML = x;        
+
+        var sel = document.getElementById("sev");
+        var text= sel.options[sel.selectedIndex].text;
+        document.getElementById("sevOp").innerHTML = text;
+
+        var y = document.getElementById("ass").value;
+        document.getElementById("assOp").innerHTML = y;
+
+        document.getElementById("desc").value = "";
+        document.getElementById("ass").value = "";
+        
+    }
+    
+}
+
+
+
