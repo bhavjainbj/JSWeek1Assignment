f1c47f545744796d2a1f9989e303e4b6ee95128a
Bhavya Jain <63778065+bhavjainbj@users.noreply.github.com>
2023-01-24 11:54:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 var closebtn = document.getElementById("btnClose");
 var deletebtn = document.getElementById("btnDelete");
+var badge = document.getElementById("badge");
+var idName = document.getElementById("idName");
 
 closebtn.hidden = true;
 deletebtn.hidden = true;
+badge.hidden = true;
+idName.hidden = true;
 
+let guid = () => {
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
 
 function checkForBlank(){
     
@@ -20,6 +33,13 @@ function checkForBlank(){
         return false;
     }
     else{
+        idName.hidden = false;
+        
+        var i = guid();
+        document.getElementById("id").innerHTML = i;
+
+        badge.hidden = false;
+
         var x = document.getElementById("desc").value;
         document.getElementById("descOp").innerHTML = x;        
 
