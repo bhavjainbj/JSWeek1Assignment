b20ad1e2028b2a746abe0d28d48b0dc17e03a9f8
Bhavya Jain <63778065+bhavjainbj@users.noreply.github.com>
2023-01-24 15:57:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
+// var Nextbtn = document.getElementById("btnNext");
+// var deletebtn = document.getElementById("btnDelete");
+// var badge = document.getElementById("badge");
+// var idName = document.getElementById("idName");
+
+// var DNextbtn = document.getElementById("DbtnNext");
+// var Ddeletebtn = document.getElementById("DbtnDelete");
+// var Dbadge = document.getElementById("Dbadge");
+// var DidName = document.getElementById("DidName");
+
+// Nextbtn.hidden = true;
+// deletebtn.hidden = true;
+// badge.hidden = true;
+// idName.hidden = true;
+
+
+// Dbadge.hidden = true;
+// DidName.hidden = true;
+
+// let guid = () => {
+//     let s4 = () => {
+//         return Math.floor((1 + Math.random()) * 0x10000)
+//             .toString(16)
+//             .substring(1);
+//     }
+//     //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+//     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+// }
+
+// function checkForBlank(){
+    
+    
+
+//     if(document.getElementById('desc').value == ""){
+//         alert("Enter the Description!!");
+//         document.getElementById('desc').style.borderColor = 'red';
+//         return false;
+//     }
+//     else if(document.getElementById('ass').value == ""){
+//         alert("Enter the Assigned To!!");
+//         document.getElementById('ass').style.borderColor = 'red';
+//         return false;
+//     }
+//     else{
+//         idName.hidden = false;
+//         DidName.hidden = false;
+
+//         var i = guid();
+//         document.getElementById("id").innerHTML = i;
+//         // document.getElementById("Did").innerHTML = i;
+
+//         badge.hidden = false;
+//         Dbadge.hidden = false;
+
+//         var x = document.getElementById("desc").value;
+//         document.getElementById("descOp").innerHTML = x;        
+//         // document.getElementById("DdescOp").innerHTML = x;        
+
+//         var sel = document.getElementById("sev");
+//         var text= sel.options[sel.selectedIndex].text;
+//         document.getElementById("sevOp").innerHTML = text;
+//         // document.getElementById("DsevOp").innerHTML = text;
+
+//         var y = document.getElementById("ass").value;
+//         document.getElementById("assOp").innerHTML = y;
+//         // document.getElementById("DassOp").innerHTML = y;
+
+//         Nextbtn.hidden = false;
+//         deletebtn.hidden = false;
+        
+
+//         document.getElementById("desc").value = "";
+//         document.getElementById("ass").value = "";
+        
+//     }
+    
+// }
+
+
+
 var closebtn = document.getElementById("btnClose");
 var deletebtn = document.getElementById("btnDelete");
 var badge = document.getElementById("badge");
@@ -59,6 +139,3 @@ function checkForBlank(){
     }
     
 }
-
-
-
